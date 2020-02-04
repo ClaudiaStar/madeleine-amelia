d5e5ffded219b21e066973673c571f92344990dc
Claudia Estrada <cgestrada518@gmail.com>
2020-02-04 15:04:39 -0800

    minor style updates to contact button

--- src/components/ParallaxEffect/ParallaxEffect.js
+++ src/components/ParallaxEffect/ParallaxEffect.js
@@ -14,7 +14,8 @@ const insideStyles = {
   position: "absolute",
   top: "50%",
   left: "50%",
-  transform: "translate(-50%,-50%)"
+  transform: "translate(-50%,-50%)",
+  width: "fit-content"
 };
 const buttonStyles = {
   display: "inline-block"
